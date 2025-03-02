dcac0443514de2b03ca57ce9c829a1d5e5ddca2f
Anderson Paulo <andersonpauloti@gmail.com>
2025-03-02 18:14:13 -0300

    Adicionando outras opções de produtos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import MenuHorizontal from "./scripts/menuHorizontal";
-import logo from "./assets/oie_transparent.png";
-import Search from "./assets/procurar.svg";
-import Share from "./assets/compartilhar.png";
-import Add from "./assets/cruz.svg";
-import Shooping from "./assets/carrinho-de-compras.svg";
-import Home from "./assets/icon.png";
-import Itens from "./assets/Frango_Assado.png";
+import './App.css'
+import MenuHorizontal from "./scripts/menuHorizontal"
+import Promotion from './scripts/products/promotion'
+import Chickens from './scripts/products/chickens'
+import Dessert from './scripts/products/dessert'
+import Drinks from './scripts/products/drinks'
+import FollowUp from './scripts/products/followUp'
+import Ribs from './scripts/products/ribs'
+import Sousage from './scripts/products/sausage'
+//import { Link, Element } from 'react-scroll'
+import logo from "./assets/oie_transparent.png"
+import Search from "./assets/procurar.svg"
+import Share from "./assets/compartilhar.png"
+
+import Shooping from "./assets/carrinho-de-compras.svg"
+import Home from "./assets/icon.png"
 
 
 
@@ -14,7 +21,7 @@ import Itens from "./assets/Frango_Assado.png";
 
 function App() {
 
-const menuItems = ['PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','CUSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','CUSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','CUSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','CUSTELINHAS','SOBREMESSAS','BEBIDAS']
+const menuItems = ['PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','COSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','COSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','COSTELINHAS','SOBREMESSAS','BEBIDAS','PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','COSTELINHAS','SOBREMESSAS','BEBIDAS']
   
 
   return (
@@ -30,55 +37,21 @@ const menuItems = ['PROMOÇÃO','FRANGOS','LINGUIÇAS','ACOMPANHAMENTOS','CUSTEL
           <div className="opened">Abre Hoje às 10h</div>          
           <div className="perfil_button">Perfil da loja</div>
         </div>
-        <div className='fhatherScroll' >
-          
-              <MenuHorizontal items = {menuItems}/>
-             
+        <div className='fhatherScroll' >      
+
+            <MenuHorizontal items = {menuItems}/>             
           
         </div>
-        <div className="name_list">PROMOÇÃO DO DIA</div>
-          <hr className="hr_prime"></hr>
-        <div className="intens">
-          <div className="products">
-           
-            <span className="discrition">Frango/Batatas</span>
-            <span className="price">R$ 24,99</span>
-            <span className="image"><img src={Itens} alt="" ></img></span>
-            <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
-            <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
-            <hr className="divisor_hr"></hr>
-          </div>
+        
+          <Promotion/>
+          <Chickens/>
+          <Sousage/>
+          <FollowUp/>
+          <Ribs/>
+          <Dessert/>
+          <Drinks/>
           
-          <div className="products">
-            
-            <span className="discrition">Frango/Batatas/Linguiça</span>
-            <span className="price">R$ 32,99</span>
-            <span className="image"><img src={Itens} alt=""></img></span>
-            <span className="add_item"><img src={Add} alt=""></img><span className="unity">0</span></span>
-            <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
-            <hr className="divisor_hr"></hr>
-          </div>
-         
-          <div className="products">
-          <span className="discrition">Frango/Batatas/Salpicão</span>
-            <span className="price">R$ 44,99</span>
-            <span className="image"><img src={Itens} alt=""></img></span>
-            <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
-            <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
-            <hr className="divisor_hr"></hr>
-          </div>
-
-          <div className="products">
-          <span className="discrition">Frango/Costelinha</span>
-            <span className="price">R$ 44,99</span>
-            <span className="image"><img src={Itens} alt=""></img></span>
-            <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
-            <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
-            <hr className="divisor_hr"></hr>
-          </div>
           
-        </div>
-
          
        
         <div className="footer">
---
+++ src/scripts/products/followUp.js
@@ -0,0 +1,63 @@
+import Add from "../../assets/cruz.svg"
+import Itens from "../../assets/Frango_Assado.png"
+import Shooping from "../../assets/carrinho-de-compras.svg"
+
+const products = ()=>{
+
+    return(
+        
+        <div className="name_list">
+        
+        <span className="name">ACOMPANHAMENTOS</span>
+
+        <hr className="hr_prime"></hr>
+
+            <div className="intens">
+           
+          
+          
+                <div className="products">
+                
+                    <span className="discrition">Frango/Batatas</span>
+                    <span className="price">R$ 24,99</span>
+                    <span className="image"><img src={Itens} alt="" ></img></span>
+                    <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
+                    <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
+                    <hr className="divisor_hr"></hr>
+                </div>
+            
+                <div className="products">
+                    
+                    <span className="discrition">Frango/Batatas/Linguiça</span>
+                    <span className="price">R$ 32,99</span>
+                    <span className="image"><img src={Itens} alt=""></img></span>
+                    <span className="add_item"><img src={Add} alt=""></img><span className="unity">0</span></span>
+                    <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
+                    <hr className="divisor_hr"></hr>
+                </div>
+            
+                <div className="products">
+                <span className="discrition">Frango/Batatas/Salpicão</span>
+                    <span className="price">R$ 44,99</span>
+                    <span className="image"><img src={Itens} alt=""></img></span>
+                    <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
+                    <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
+                    <hr className="divisor_hr"></hr>
+                </div>
+
+                <div className="products">
+                <span className="discrition">Frango/Costelinha</span>
+                    <span className="price">R$ 44,99</span>
+                    <span className="image"><img src={Itens} alt=""></img></span>
+                    <span className="add_item"><img src={Add} alt="" ></img><span className="unity">0</span></span>
+                    <span className="add_shopping"><img src={Shooping} alt="" ></img></span>
+                    <hr className="divisor_hr"></hr>
+                </div>
+            </div>
+        </div>
+       
+    )
+
+
+}
+export default products
